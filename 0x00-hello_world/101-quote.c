0e178fa7117a6e7ab07390585b7c57d04c6113cf
joanweru <nyokabiweru44@gmail.com>
2023-06-15 18:34:32 +0300

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,7 @@
 /**
  * main - prints "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
- *                          
-                 followed by a new line, to the SE.
+ * followed by a new line, to the SE.
  * Return: Always 0 if code was a sucess
  */
 int main(void)
