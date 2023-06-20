dc2d6411da0601712f2f1040ee82e60a553fc491
joanweru <nyokabiweru44@gmail.com>
2023-06-20 19:39:26 +0300

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and ....#
+ * @int1: first integer to be picked
+ * @int2: second int to be picked
+ * Return: returns the result
+ */
+
+int add(int int1, int int2)
+{
+	return (int1, int2);
+}
+
+
