45e3cff168790e375ca33887c181e3279dd27194
joanweru <nyokabiweru44@gmail.com>
2023-06-29 21:22:32 +0300

    corrected

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,23 @@
  */
 char *leet(char *str)
 {
-	int i = 0, j;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	int i, j;
 
-	while (str[i])
+	char *x = "aAeEoOtTlL";
+	char *y = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j <= 7; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (str[i] == leet[j] ||
-					str[i] - 32 == leet[j])
-				str[i] = j + '0';
+			if (str[i] == x[j])
+			{
+				str[i] = y[j];
+			}
 		}
 
-		i++;
+
 	}
 
-	return (str);
+
 }
