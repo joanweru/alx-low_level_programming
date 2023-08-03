489d7aded614460690c03c841f97a713e98aab57
joanweru <joanweru77@gmail.com>
2023-08-03 18:58:20 +0300

    100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,13 +10,11 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int num;
 
-	num =  0x01;
+	if (index > 64)/*siku kuu*/
+		return (-1);/*hope*/
 
-	num = ~(num << index);
-
-	if (num == 0x00) /*heeey*/
-		return (-1);/*intersting*/
-
+	num = ~(1 << index);
 	*n &= num;
+
 	return (1);
 }
